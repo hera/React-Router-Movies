e7e753e02a15082138295270be8fbd689213eed3
Alexander Heraimenka <a.heraimenka@gmail.com>
2020-05-18 15:46:33 -0700

    Use MovieCard component

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,33 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import MovieCard from './MovieCard';
 
 const MovieList = props => {
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
-      ))}
-    </div>
-  );
-}
-
-function MovieDetails({ movie }) {
-  const { title, director, id, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <Link to={`/movie/${id}`}><h2>{title}</h2></Link>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
+        <MovieCard key={movie.id} movie={movie} />
       ))}
     </div>
   );
